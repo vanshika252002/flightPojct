fec53b000770b7e9c015b5a59aabdc1e7d339489
Vanshika Rawat <vanshikainps25@gmail.com>
2025-04-11 18:38:05 +0530

    chore: added spacing

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,7 +3,7 @@ import searchLogo from './icons/searchLogo.png';
 import userLogo from './icons/userLogo.png';
 import google from './icons/google.png';
 import flightLogo from './icons/flightLogo.png';
-import flightroute from './icons/flightroute.svg';
+import flightroute from './icons/flightroute.svg'; 
 import liveairplane from './icons/liveairplane.svg';
 import airports from './icons/airports.svg';
 import flighthistory from './icons/flighthistory.svg';
